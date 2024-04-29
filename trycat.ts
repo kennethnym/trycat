fd470cba83ef40319a7d4d2d6d3246b7456715b0
Kenneth <kennethnym@outlook.com>
2024-04-29 18:42:03 +0100

    add doc string

--- trycat.ts
+++ trycat.ts
@@ -1,33 +1,148 @@
+/**
+ * Interface for the Result type.
+ */
 interface ResultBase<T, TErr> {
+	/**
+	 * A type guard that checks if this Result is Ok. If it is, the Result is cast to Ok.
+	 * @returns true if this Result is Ok, false otherwise.
+	 */
 	isOk(): this is Ok<T>
 
+	/**
+	 * A type guard that checks if this Result is Err. If it is, the Result is cast to Err.
+	 * @returns true if this Result is Err, false otherwise.
+	 */
 	isErr(): this is Err<TErr>
 
+	/**
+	 * Calls the provided function with the contained value if this Result is Ok.
+	 * Nothing is performed otherwise.
+	 *
+	 * @param f The function to be called if this Result is Ok. First arg will receive the contained value.
+	 *
+	 * @returns This Result.
+	 */
 	inspect(f: (value: T) => unknown): this
 
+	/**
+	 * Calls the provided function with the contained value if this Result is Err.
+	 * Nothing is performed otherwise.
+	 *
+	 * @param f The function to be called if this Result is Err. First arg will receive the contained error.
+	 *
+	 * @returns This Result.
+	 */
 	inspectErr(f: (error: TErr) => unknown): this
 
+	/**
+	 * If this Result is Ok, calls the given mapper function with the current contained value
+	 *  and returns a new Result containing the value returned by the mapper function.
+	 *
+	 * @param mapper The function that maps the current contained value to another value.
+	 *
+	 * @returns A new Result object containing the value returned by the mapper function, or this if this is Err.
+	 */
 	map<TNew>(mapper: (value: T) => TNew): this | Result<TNew, TErr>
 
+	/**
+	 * If this Result is Ok, it does the same thing as {@link map}.
+	 * Otherwise, returns the given default value.
+	 *
+	 * @param def The default value to be returned if this Result is Err.
+	 * @param mapper The function that maps the current contained value to another value if this Result is Ok.
+	 *
+	 * @returns A new Result object containing the value returned by the mapped function if this is Ok, otherwise the default value.
+	 */
 	mapOr<TNew, TDef>(def: TDef, mapper: (value: T) => TNew): TNew | TDef
 
+	/**
+	 * Same as {@link mapOr}, except if this is Err, the default value is returned lazily by the first callback
+	 * which receives the contained error and returns the default value.
+	 *
+	 * @param errMapper The function that maps the contained error value to a default value if this Result is Err.
+	 * @param mapper The function that maps the contained value to a new value if this Result is Ok.
+	 *
+	 * @returns The value returned by the error mapper if this is Err; The value returned by the value mapper if this is Ok.
+	 */
 	mapOrElse<TNew, TDef>(errMapper: (error: TErr) => TDef, mapper: (value: T) => TNew): TNew | TDef
 
-	mapErr<TNewErr>(this: Result<T, TErr>, mapper: (error: TErr) => TNewErr): this | Result<T, TNewErr>
+	/**
+	 * If this Result is Err, calls the given mapper with the current error value,
+	 * then returns a new Result containing the new error value returned by the mapper.
+	 * Otherwise, nothing is performed and this is returned.
+	 *
+	 * @param mapper The function that maps the contained error value to another error value.
+	 *
+	 * @returns A new Result containing the error value returned by the mapper if this is Err, or this Result otherwise.
+	 */
+	mapErr<TNewErr>(mapper: (error: TErr) => TNewErr): this | Result<T, TNewErr>
 
+	/**
+	 * @returns the given Result if this Result is Err, otherwise this.
+	 */
 	or<TOther, TOtherErr, TR extends Result<TOther, TOtherErr>>(res: TR): this | TR
+
+	/**
+	 * Calls the given callback with the contained error value if this Result is Err.
+	 *
+	 * @returns the Result returned by the given callback, otherwise this Result
+	 */
 	orElse<TOther, TOtherErr, TR extends Result<TOther, TOtherErr>>(op: (error: TErr) => TR): this | TR
 
+	/**
+	 * @returns the given Result if this Result is Ok, otherwise this.
+	 */
 	and<TOther, TOtherErr, TR extends Result<TOther, TOtherErr>>(res: TR): this | TR
+
+	/**
+	 * Calls the given callback with the contained value if this Result is Ok.
+	 *
+	 * @returns the Result returned by the given callback, otherwise this Result
+	 */
 	andThen<TOther, TOtherErr, TR extends Result<TOther, TOtherErr>>(op: (value: T) => TR): this | TR
 
+	/**
+	 * @returns the contained value if this Result is Ok.
+	 * @throws if this Result is Err
+	 */
 	unwrap(): T
+
+	/**
+	 * @returns the contained value if this Result is Ok or the given value otherwise.
+	 */
 	unwrapOr<TDef>(def: TDef): T | TDef
+
+	/**
+	 * Calls the given function with the contained error value and returns the value returned by the function if this Result is Err. Otherwise, returns the contained value.
+	 *
+	 * @returns The contained value if this Result is Ok or the value returned by the given function otherwise.
+	 */
 	unwrapOrElse<TDef>(op: (error: TErr) => TDef): T | TDef
+
+	/**
+	 * Returns the contained value if this Result is Ok, otherwise throws with the given error message.
+	 *
+	 * @param The error message to be displayed if this Result is Err.
+	 *
+	 * @returns The contained value if this Result is Ok.
+	 * @throws An error with the given error message if this Result is Err.
+	 */
 	expect(message: string): T
 
+	/**
+	 * @returns the contained error value if this Result is Err.
+	 * @throws the contained value if this Result is Ok
+	 */
 	unwrapErr(): TErr
 
+	/**
+	 * Returns the contained error value if this Result is Err, otherwise throws with the given error message.
+	 *
+	 * @param The error message to be displayed if this Result is Ok.
+	 *
+	 * @returns The contained error value if this Result is Err.
+	 * @throws An error with the given error message if this Result is Ok.
+	 */
 	expectErr(message: string): TErr
 }
 
@@ -195,6 +310,10 @@ function err<TErr>(error: TErr): Err<TErr> {
 	return new Err(error)
 }
 
+/**
+ * Calls the given function, catches any thrown error into an Err,
+ * and wraps the returned value with an Ok if nothing goes wrong.
+ */
 function trys<T>(fn: () => T): Result<T, unknown> {
 	try {
 		return ok(fn())
@@ -203,6 +322,16 @@ function trys<T>(fn: () => T): Result<T, unknown> {
 	}
 }
 
+/**
+ * Stores the result of the given {@link Promise} into a {@link Result}.
+ * If the promise fails, Err is returned. If the promise succeeds, Ok is returned.
+ *
+ * @example
+ * const res = await tryp(fetch("/my/api")).mapErr((err) => apiError(err))
+ * if (res.isErr()) {
+ *   return err(res.error)
+ * }
+ */
 function tryp<T>(promise: Promise<T>): Promise<Result<T, unknown>> {
 	return promise.then((value) => ok(value)).catch((e) => err(e))
 }
